73b5232b519cdecbedd1fc95065da48ea048cd42
Jorge052011 <jimunozacuna+1@gmail.com>
2026-02-16 05:13:15 +0000

    se copia base de datos

--- DistribucionApp/settings.py
+++ DistribucionApp/settings.py
@@ -33,7 +33,6 @@ CSRF_TRUSTED_ORIGINS = [
     # ✅ TU HOST EXACTO (del navegador)
     "https://ubiquitous-telegram-r4w677p5p7j4fr5r-8000.app.github.dev",
     "http://100.71.205.69:8000",
-
     "http://127.0.0.1:8000",
     "http://localhost:8000",
     "https://localhost:8000",
